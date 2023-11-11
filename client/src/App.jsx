bade3ce467b9147c94dfd07d7ca69c08f6b63271
losarwarameya <losarwarah@gmail.com>
2023-11-11 14:20:39 +0530

    installed tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserRegisterPage from "./components/UserRegisterPage";
 import UserLoginPage from "./components/UserLoginPage";
+import HomePage from "./components/HomePage";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/register" element={<UserRegisterPage />} />
         <Route path="/login" element={<UserLoginPage />} />
+        <Route path="/home" element={<HomePage />}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import Hero from './Hero'
+
+const HomePage = () => {
+  return (
+    <div>
+        <Sidebar />
+        <Hero />
+    </div>
+  )
+}
+
+export default HomePage
